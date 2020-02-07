51e6697a2d5fa3e78ace5f68d51c33ae085ffb3a
Yuri Delgado <yuri@fintecimal.com>
2020-02-06 18:19:10 -0600

    handling user authentication and request authorization

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :profiles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  post 'authenticate', to: 'authentication#authenticate'
 end
