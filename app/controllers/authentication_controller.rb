7394952a9c5e50dc01deab16264b0d14875ceab0
Yuri Delgado <yuri.m.delgado@gmail.com>
2020-05-31 11:30:22 -0500

    adding display name to auth response

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -3,9 +3,9 @@ class AuthenticationController < ApplicationController
 
   def authenticate
     command = AuthenticateUser.call(user_params[:email], user_params[:password])
-
+    username = 
     if command.success?
-      render json: { auth_token: command.result }
+      render json: { auth_token: command.result, display_name: display_name }
     else
       render json: { error: command.errors }, status: :unauthorized
     end
@@ -14,5 +14,10 @@ class AuthenticationController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:email, :password)
+  end
+
+  def display_name
+    user = User.find_by(email: user_params[:email])
+    user.profile.name
   end
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,7 @@
 class Profile < ApplicationRecord
   belongs_to :user
 
+  def full_name
+    "#{name} #{last_name}"
+  end
 end
